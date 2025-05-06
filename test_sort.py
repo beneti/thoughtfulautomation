bbd9c347729f35dd9b78e3469914571e5b80d33b
Fred Beneti <beneti@beneti.com.br>
2025-05-06 18:46:45 +0000

    Added tests

---
+++ test_sort.py
@@ -0,0 +1,22 @@
+import unittest
+
+from main import sort
+
+
+class TestSort(unittest.TestCase):
+
+  def test_standard(self):
+    self.assertEqual(sort(1, 1, 1, 1), "STANDARD")
+
+  def test_special_bulky(self):
+    self.assertEqual(sort(150, 1, 1, 1), "SPECIAL")
+    self.assertEqual(sort(1, 150, 1, 1), "SPECIAL")
+    self.assertEqual(sort(1, 1, 150, 1), "SPECIAL")
+    self.assertEqual(sort(140, 140, 140, 1), "SPECIAL")
+
+  def test_special_heavy(self):
+    self.assertEqual(sort(1, 1, 1, 20), "SPECIAL")
+
+  def test_rejected(self):
+    self.assertEqual(sort(150, 1, 1, 20), "REJECTED")
+    self.assertEqual(sort(140, 140, 140, 20), "REJECTED")
